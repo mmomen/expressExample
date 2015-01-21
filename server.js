8aa2dd9c7706f8820a49453059b59383fc9118d6
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-20 23:22:34 -0800

    Jade exampeles

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var app = Express();
 
 app.use(Express.static(__dirname+"/public"));
 
+// app.get("/", function(req, res, next) {
+//   res.sendFile("./views/index.html", {
+//     root: __dirname
+//   });
+// });
+
+app.engine('jade', require('jade').__express);
+app.set("view engine", "jade");
+
 app.get("/", function(req, res, next) {
-  res.sendFile("./views/index.html", {
-    root: __dirname
-  });
+  res.render('index', {header: "Jade rocks!", destination: 'far'});
 });
 
 var port = "8080";
