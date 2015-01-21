9e179068fd461279d508247e93204f87e72f0eeb
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-21 10:12:31 -0800

    Using router and exporting it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var Express = require ('express');
 var app = Express();
+var users = require('./routes/users');
 
 app.use(Express.static(__dirname+"/public"));
 
@@ -12,9 +13,15 @@ app.use(Express.static(__dirname+"/public"));
 app.engine('jade', require('jade').__express);
 app.set("view engine", "jade");
 
-app.get("/", function(req, res, next) {
-  res.render('index', {header: "Jade rocks!", destination: 'far'});
+// app.get("/", function(req, res, next) {
+//   res.render('index', {header: "Jade rocks!", destination: 'far'});
+// });
+
+app.get('/', function(req, res, next) {
+  res.send("<h1>Hellow world</h1>");
 });
+
+app.use("/users", users);
 
 var port = "8080";
 var server = app.listen(port);
